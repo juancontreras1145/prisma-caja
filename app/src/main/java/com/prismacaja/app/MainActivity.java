27ab4ce83d22609ca6c705d3f0b9c439492ac749
Juan Contreras <juandcontreras1145@users.noreply.github.com>
2026-06-07 00:21:39 -0400

    Actualizar app local e importacion inicial

--- app/src/main/java/com/prismacaja/app/MainActivity.java
+++ app/src/main/java/com/prismacaja/app/MainActivity.java
@@ -65,13 +65,13 @@ public class MainActivity extends Activity {
     }
 
     private void hideSystemBars() {
+        // No ocultamos la barra inferior de Android.
+        // Así quedan visibles Atrás / Inicio / Recientes.
+        getWindow().setStatusBarColor(android.graphics.Color.parseColor("#07111f"));
+        getWindow().setNavigationBarColor(android.graphics.Color.parseColor("#07111f"));
+
         getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         );
     }
 
